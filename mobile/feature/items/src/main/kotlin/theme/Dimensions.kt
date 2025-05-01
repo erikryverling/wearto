aa594b75e84e816af106e72fda7b95b7e949b900
Erik R. Yverling <erik.r@yverling.se>
2025-05-01 10:58:03 +0200

    Fix sync button animation

--- mobile/feature/items/src/main/kotlin/theme/Dimensions.kt
+++ mobile/feature/items/src/main/kotlin/theme/Dimensions.kt
@@ -5,5 +5,5 @@ import androidx.compose.ui.unit.dp
 internal val ItemCardElevation = 1.dp
 internal val AddItemSize = 98.dp
 
-internal const val IconAnimationRotation = -540f
+internal const val IconAnimationRotation = 540f
 internal const val IconAnimationDurationInMillis = 1000
